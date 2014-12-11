933803434c498286d7068463d934abbbe02eb8ca
Kyle Mathews <mathews.kyle@gmail.com>
2014-12-10 18:13:19 -0800

    Add module

---
+++ index.js
@@ -0,0 +1,10 @@
+module.exports = function(src, callback) {
+  var img = new Image();
+  img.onload = function() {
+    callback(true);
+  };
+  img.onerror = function() {
+    callback(false);
+  };
+  img.src = src;
+};

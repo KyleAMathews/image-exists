56be7864dd117736fe9a7fd7c80815b24f83c19a
Kyle Mathews <mathews.kyle@gmail.com>
2014-12-18 16:08:02 -0800

    Cache checks

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
+var checks = {};
+
 module.exports = function(src, callback) {
+  if (src in checks) {
+    return callback(checks[src]);
+  }
+
   var img = new Image();
+
   img.onload = function() {
+    checks[src] = true;
     callback(true);
   };
+
   img.onerror = function() {
+    checks[src] = false;
     callback(false);
   };
+
   img.src = src;
 };
